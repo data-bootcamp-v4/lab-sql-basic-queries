d74d88a44d462d898f8f2c33728abaf2dde157fb
Javivi <161762434+JavierVillares@users.noreply.github.com>
2024-04-30 15:42:15 +0200

    hello there

---
+++ sakila-data.sql
@@ -0,0 +1,52 @@
+USE sakila;
+
+SHOW tables;
+
+SELECT *
+FROM actor;
+
+SELECT *
+FROM address;
+
+SELECT *
+FROM category;
+
+SELECT *
+FROM city;
+
+SELECT *
+FROM country;
+
+SELECT *
+FROM customer;
+
+SELECT *
+FROM film;
+
+SELECT *
+FROM film_actor;
+
+SELECT *
+FROM film_category;
+
+SELECT *
+FROM film_text;
+
+SELECT *
+FROM inventory;
+
+SELECT *
+FROM language;
+
+SELECT *
+FROM payment;
+
+SELECT *
+FROM rental;
+
+SELECT *
+FROM staff;
+
+SELECT *
+FROM store;
+
