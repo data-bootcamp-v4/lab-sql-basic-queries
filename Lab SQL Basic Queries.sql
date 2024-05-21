c6ccce5a4a5d1fbfafedf4430a5ac29ea98645ff
dsteede <daniellesteede@gmail.com>
2024-05-21 19:48:09 -0400

    Basic Queries updated

--- Lab SQL Basic Queries.sql
+++ Lab SQL Basic Queries.sql
@@ -1,6 +1,9 @@
 USE sakila;
 
--- Number two
+-- Number one -- Display all available tables in the Sakila database.
+SHOW TABLES;
+
+-- Number two -- Retrieve all the data from the tables actor, film and customer.
 
 Select *
 FROM actor;
@@ -11,45 +14,38 @@ FROM film;
 Select *
 FROM customer;
 
--- Number 3
+-- Number 3 -- Retrieve the following columns from their respective tables
 
 SELECT title
 FROM film;
 
-SELECT name AS `language`
+SELECT name AS language 
 FROM language;
 
 SELECT first_name
 FROM staff;
 
--- Number 4
+-- Number 4 -- Retrieve unique release years.
 
-SELECT DISTINCT(release_year)
-FROM film;
-
--- Number Five
-
-SELECT COUNT(store_id)
-FROM store;
-
-SELECT COUNT(staff_id)
-FROM staff;
-
-SELECT DISTINCT(COUNT(inventory_id))
-from rental;
-
-SELECT COUNT(DISTINCT(last_name))
-FROM actor;
-
--- Number Six
-
-SELECT *
+SELECT DISTINCT YEAR(release_year) AS release_year 
 FROM film
-ORDER BY length DESC
+
+-- Number Five -- Counting records for database insights
+
+SELECT COUNT(*) AS number_of_stores FROM sakila.store;
+
+SELECT 
+    (SELECT COUNT(*) FROM film) AS total_films,
+    (SELECT COUNT(*) FROM inventory WHERE NOT inventory_id IN (SELECT inventory_id FROM rental)) AS films_available_for_rent,
+    (SELECT COUNT(*) FROM rental) AS films_rented;
+
+SELECT COUNT(DISTINCT last_name) AS number_of_distinct_last_names FROM actor;
+
+-- Number Six -- Retrieve the 10 longest films.
+
+SELECT * FROM film ORDER BY length DESC 
 LIMIT 10;
 
--- Number 7
+-- Number 7 -- Retrieve all actors with the first name "SCARLETT".
 
-SELECT first_name
-FROM actor
-WHERE first_name = `SCARLETT`;
+SELECT * FROM actor WHERE first_name = 'SCARLETT';
