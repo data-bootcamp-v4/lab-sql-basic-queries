010f39edfda751c912e21befe9f114fa7794cdd5
trjno <168582583+trjno@users.noreply.github.com>
2024-05-22 16:03:44 -0400

    Add files via upload

--- Lab20may24.sql
+++ Lab20may24.sql
@@ -20,3 +20,29 @@ FROM language;
 -- 3.3 List of first names of all employees from the staff table
 SELECT *
 FROM staff;
+-- 4 Retrieve unique release years.
+SELECT distinct release_year FROM film;
+-- 5 Counting records for database insights:
+-- 5.1 Determine the number of stores that the company has.
+SELECT COUNT(*) FROM store;
+-- 5.2 Determine the number of employees that the company has.
+SELECT COUNT(*) FROM staff;
+-- 5.3 Determine how many films are available for rent and how many have been rented.
+SELECT COUNT(*) FROM rental WHERE return_date is null;
+SELECT COUNT(*) FROM rental; 
+SELECT COUNT(*) FROM rental WHERE return_date is not null;
+-- 5.4 Determine the number of distinct last names of the actors in the database.
+SELECT COUNT(distinct last_name) FROM actor;
+-- 6 Retrieve the 10 longest films.
+SELECT * FROM film ORDER BY length DESC Limit 10;
+-- Use filtering techniques in order to:
+-- 7.1 Retrieve all actors with the first name "SCARLETT".
+SELECT * FROM actor WHERE first_name = "SCARLETT";
+-- BONUS:
+
+-- 7.2 Retrieve all movies that have ARMAGEDDON in their title and have a duration longer than 100 minutes.
+SELECT * FROM film WHERE title like '%ARMAGEDDON%';
+SELECT * FROM film WHERE length >100;
+-- Hint: use LIKE operator. More information here.
+-- 7.3 Determine the number of films that include Behind the Scenes content
+SELECT * 
