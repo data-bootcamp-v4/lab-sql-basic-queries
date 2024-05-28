a26329355dabbbc4c31384cad7e9e605dfcb8d8e
Ryanvandenakker <vandenakkerryan@gmail.com>
2024-05-28 16:31:03 +0200

    Lab Solved

--- lab-sql-basic-queries.sql
+++ lab-sql-basic-queries.sql
@@ -22,12 +22,12 @@ SELECT DISTINCT release_year FROM sakila.film;
 
 -- 5. Counting records for database insights
 -- 5.1 Determine the number of stores that the company has
-SELECT COUNT(store_id) FROM sakila.store;
+SELECT COUNT(DISTINCT store_id) FROM sakila.store;
 -- 5.2 Determine the number of employees that the company has
-SELECT COUNT(staff_id) FROM sakila.staff;
+SELECT COUNT(DISTINCT staff_id) FROM sakila.staff;
 -- 5.3 Determine how many films are available for rent and how many have been rented.
-SELECT COUNT(inventory_id) FROM sakila.inventory;
-SELECT COUNT(rental_id) FROM sakila.rental;
+SELECT COUNT(DISTINCT inventory_id) FROM sakila.inventory;
+SELECT COUNT(DISTINCT rental_id) FROM sakila.rental;
 -- 5.4 Determine the number of distinct last names of the actors in the database.
 SELECT COUNT(DISTINCT last_name) FROM sakila.actor;
 
@@ -42,7 +42,7 @@ SELECT * FROM sakila.actor
 WHERE first_name = "SCARLETT";
 -- 7.2 Retrieve all movies that have ARMAGEDDON in their title and have a duration longer than 100 minutes.
 SELECT * FROM sakila.film
-WHERE title LIKE '%Armageddon%' AND length > 100;
+WHERE title LIKE '%ARMAGEDDON%' AND length > 100;
 -- 7.3 Determine the number of films that include Behind the Scenes content
 SELECT COUNT(film_id) FROM sakila.film
 WHERE special_features LIKE '%Behind the Scenes%';
