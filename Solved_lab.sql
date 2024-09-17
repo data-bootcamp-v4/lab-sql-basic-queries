648c3c560db8fe745bd8c5c2674eee58ec857866
Ana <ananofuentes7@outlook.es>
2024-09-17 22:18:28 +0200

    sql_2

--- Solved_lab.sql
+++ Solved_lab.sql
@@ -1,40 +1,37 @@
--- 1
+-- 1 Use the sakila database and show all tables
 use sakila;
 show tables;
--- 2
+-- 2 Retrieve all data from actor, film, and customer tables
 select *
 from actor, film, customer;
 -- 3
--- 3.1
+-- 3.1 Retrieve titles of all films
 select title
 from film;
--- 3.2
+-- 3.2 Retrieve languages used in films with alias 'language'
 select name as language
 from language
 ;
--- 3.3
+-- 3.3 Retrieve first names of all employees
 select first_name
 from staff;
--- 4
+-- 4 Retrieve unique release years
 select distinct release_year
 from film;
 -- 5 
 -- 5.1 Determine the number of stores that the company has.
-select count(store_id)
+select count(distinct store_id)
 from store;
 
 -- 5.2 Determine the number of employees that the company has.
-select count(staff_id)
+select count(distinct staff_id)
 from staff;
 -- 5.3 Determine how many films are available for rent and how many have been rented.
-
-    
-select count(distinct(inventory_id)) as available_films, 
-		count(distinct(rental_id)) as rented_times
-from rental;
+select COUNT(*) as available_films from inventory where inventory_id not in (select inventory_id from rental);
+select COUNT(*) as rented_times from rental;
 
 -- 5.4 Determine the number of distinct last names of the actors in the database.
-select count(distinct(last_name)) as n_last_names
+select count(distinct last_name) as n_last_names
 from actor;
 -- 6 Retrieve the 10 longest films.
 select title, length
@@ -56,6 +53,6 @@ where title like '%ARMAGEDDON%' and length > 100;
 
 ##7.3 Determine the number of films that include Behind the Scenes content
 
-select distinct count(title) as number_behind
+select count(title) as number_behind
 from film
 where special_features like '%Behind the Scenes%';
