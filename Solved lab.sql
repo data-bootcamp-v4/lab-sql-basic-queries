a010968ec60904fd1edc609dfd016decdeed29c2
Elena Vilkoyt <158162130+e-vilkoyt@users.noreply.github.com>
2024-09-17 15:05:09 +0200

    Update Solved lab.sql

--- Solved lab.sql
+++ Solved lab.sql
@@ -13,3 +13,11 @@ SELECT title FROM film
 ORDER BY length DESC
 LIMIT 10;
 SELECT * FROM actor WHERE first_name = 'SCARLETT';
+-- Número de películas disponibles para alquilar
+SELECT COUNT(*) AS available_films
+FROM inventory
+WHERE inventory_id NOT IN (SELECT inventory_id FROM rental WHERE return_date IS NULL);
+SELECT COUNT(DISTINCT inventory_id) AS rented_films
+FROM rental;
+SELECT COUNT(DISTINCT last_name) AS distinct_last_names
+FROM actor;
