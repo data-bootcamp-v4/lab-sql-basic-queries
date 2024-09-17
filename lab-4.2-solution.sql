cc47a4d28bbf1bf4ed8f84035c29cb925c1fe81b
Almudena M. Castro <almu@Almudenas-MacBook-Pro.local>
2024-09-18 00:21:43 +0200

    lab solution

--- lab-4.2-solution.sql
+++ lab-4.2-solution.sql
@@ -11,12 +11,12 @@ SELECT * FROM sakila.customer;
 -- 	- 3.1 Titles of all films from the `film` table
 SELECT sf.title from sakila.film as sf;
 
--- 	- 3.2 List of languages used in films, with the column aliased as `language` from the `language` table
+--- 3.2 List of languages used in films, with the column aliased as `language` from the `language` table
 SELECT DISTINCT sl.name as language 
 FROM sakila.language as sl;
 -- > English, Italian, Japanese, Mandarin, French, German
 
--- 	- 3.3 List of first names of all employees from the `staff` table
+--- 3.3 List of first names of all employees from the `staff` table
 SELECT ss.first_name
 FROM sakila.staff as ss;
 -- > Mike, Jon
@@ -27,22 +27,22 @@ FROM sakila.film as sf;
 -- > 2006
 
 -- 5. Counting records for database insights:
--- 	- 5.1 Determine the number of stores that the company has.
+--- 5.1 Determine the number of stores that the company has.
 SELECT COUNT(ss.store_id)
 FROM sakila.store as ss;
 -- > 2
 
--- 	- 5.2 Determine the number of employees that the company has.
+--- 5.2 Determine the number of employees that the company has.
 SELECT COUNT(ss.staff_id)
 FROM sakila.staff as ss;
 -- > 2
 
--- 	- 5.3 Determine how many films are available for rent and how many have been rented.
+--- 5.3 Determine how many films are available for rent and how many have been rented.
 SELECT COUNT(DISTINCT si.film_id) as films, COUNT(DISTINCT si.inventory_id) as copies 
 from sakila.inventory as si;
 -- > 4581 copies of 958 different films
 
--- 	- 5.4 Determine the number of distinct last names of the actors in the database.
+--- 5.4 Determine the number of distinct last names of the actors in the database.
 SELECT COUNT(DISTINCT sa.first_name)
 FROM sakila.actor as sa;
 -- > 128 distinct names
@@ -55,7 +55,7 @@ LIMIT 10;
 
 -- 7.  Use filtering techniques in order to:
 
--- 	- 7.1 Retrieve all actors with the first name "SCARLETT".
+--- 7.1 Retrieve all actors with the first name "SCARLETT".
 SELECT * FROM sakila.actor as sa
 WHERE sa.first_name = 'SCARLETT';
 
