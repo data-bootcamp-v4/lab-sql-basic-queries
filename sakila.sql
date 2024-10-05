9cb22c5b81cff904575c323538e77d4a592511c0
jipijipi <jp.leflot@gmail.com>
2024-10-05 13:23:36 +0200

    ai check weirdness

--- sakila.sql
+++ sakila.sql
@@ -8,21 +8,18 @@ SELECT title FROM film;
 SELECT name AS language FROM language;
 SELECT first_name FROM staff;
 
-SELECT release_year FROM film;
-
 SELECT DISTINCT release_year FROM film;
 
-SELECT COUNT(*) AS store_count FROM store;
-
+SELECT COUNT(*) FROM store;
+SELECT COUNT(*) FROM staff;
 SELECT COUNT(DISTINCT film_id) FROM inventory;
 SELECT COUNT(DISTINCT inventory_id) FROM rental;
-
 SELECT COUNT(DISTINCT last_name) FROM actor;
 
 SELECT title, length FROM film ORDER BY length DESC LIMIT 10;
 
-SELECT first_name, last_name FROM actor WHERE first_name = 'Scarlett';
+SELECT first_name, last_name FROM actor WHERE first_name = 'SCARLETT';
 
 SELECT title, length FROM film WHERE title LIKE '%ARMAGEDDON%' AND length > 100;
 
-SELECT COUNT(*) FROM film WHERE special_features LIKE '%Behind%';
+SELECT COUNT(*) FROM film WHERE special_features LIKE '%Behind the Scenes%';
