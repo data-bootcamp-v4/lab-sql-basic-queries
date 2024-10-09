56e2180cc99a5a7ff219943e808885c802236020
SunMax44 <maxsonntag@posteo.de>
2024-10-09 14:45:30 +0200

    FINAL basic

---
+++ basic_queries.sql
@@ -0,0 +1,51 @@
+USE sakila;
+
+SHOW TABLES;
+
+SELECT title
+FROM film;
+
+SELECT language_id, name
+FROM language;
+
+SELECT first_name
+FROM staff;
+
+#unique release years (=1 -->2006)
+SELECT DISTINCT release_year
+FROM film;
+
+#number of stores (=2)
+SELECT COUNT(store_id)
+FROM store;
+
+#number of employees (=2)
+SELECT COUNT(staff_id)
+FROM staff;
+
+#Count films in inventory : 4581
+SELECT COUNT(film_id)
+FROM inventory;
+#Count rentals: 16044
+SELECT COUNT(rental_id)
+FROM rental;
+#number of distinct last names of actors: 121
+SELECT COUNT(DISTINCT last_name)
+FROM actor;
+
+#retrieve the 10 longest films:
+SELECT title, length
+FROM film
+ORDER BY length DESC
+LIMIT 10;
+
+#Use filtering techniques in order to:
+#7.1 Retrieve all actors with the first name "SCARLETT".
+SELECT first_name, last_name
+FROM actor
+WHERE first_name = "SCARLETT";
+
+SELECT title, length
+FROM film
+WHERE length > 100
+	AND title LIKE("%ARMAGEDDON%");
