6fbab16367fc0fdb1c15f2a967c8791b6ef1b58f
luismrtnzgl <luis.mrtnzgl@gmail.com>
2024-11-12 19:53:51 +0100

    Lab 9.1 Solved

---
+++ sakila_data_lab.sql
@@ -0,0 +1,40 @@
+USE sakila;
+
+SHOW TABLES;
+
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+
+SELECT title FROM film;
+
+SELECT name AS language FROM language;
+
+SELECT first_name FROM staff;
+
+SELECT DISTINCT release_year FROM film;
+
+SELECT COUNT(*) AS store_count FROM store;
+
+SELECT COUNT(*) AS employee_count FROM staff;
+
+SELECT COUNT(*) AS available_films FROM inventory;
+
+SELECT COUNT(*) AS rented_films FROM rental;
+
+SELECT COUNT(DISTINCT last_name) AS unique_last_names FROM actor;
+
+SELECT title, length FROM film
+ORDER BY length DESC
+LIMIT 10;
+
+SELECT * FROM actor
+WHERE first_name = 'SCARLETT';
+
+SELECT * FROM film
+WHERE title LIKE '%ARMAGEDDON%' AND length > 100;
+
+SELECT COUNT(*) AS behind_the_scenes_count FROM film
+WHERE special_features LIKE '%Behind the Scenes%';
+
+
