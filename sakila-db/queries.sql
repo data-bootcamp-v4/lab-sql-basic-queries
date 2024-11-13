a7e5ebf2504f99e428ed6f34bd81af914708a9a6
bru <brugera.herrero@gmail.com>
2024-11-13 12:29:16 +0100

    display tables

---
+++ sakila-db/queries.sql
@@ -0,0 +1,9 @@
+USE sakila;
+
+-- Display all available tables in the Sakila database.
+SHOW TABLES;
+
+-- Retrieve all the data from the tables actor, film and customer.
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
