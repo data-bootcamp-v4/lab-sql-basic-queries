06432a9db4d14bf447281faa2a8a2c80ad45e37b
Enia Lahcene Arzamendi <enialahcene1@gmail.com>
2024-11-14 18:53:58 +0000

    Solved lab

---
+++ lab.sql
@@ -0,0 +1,63 @@
+USE sakila;
+
+SHOW TABLES;
+
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+
+SELECT title FROM film;
+SELECT name AS language FROM language;
+SELECT first_name FROM staff;
+
+SELECT DISTINCT release_year
+FROM film;
+
+SELECT COUNT(*) AS number_of_stores
+FROM store;
+
+SELECT COUNT(*) AS number_of_employees
+FROM staff;
+
+SELECT COUNT(*) AS available_films
+FROM film;
+
+SELECT COUNT(*) AS rented_films
+FROM rental
+WHERE return_date IS NULL;
+
+SELECT COUNT(DISTINCT last_name) AS distinct_last_names
+FROM actor; 
+
+SELECT 
+	title,
+	length
+FROM 
+	film
+ORDER BY 
+	length DESC
+LIMIT 10;
+
+SELECT 
+    first_name, 
+    last_name 
+FROM 
+    actor 
+WHERE 
+    first_name = 'SCARLETT';
+
+SELECT
+	title,
+    length
+FROM
+	film
+WHERE
+	title LIKE '%ARMAGEDDON%'
+	AND length > 100;
+ 
+ SELECT 
+    COUNT(*) AS number_of_films_with_behind_the_scenes
+FROM 
+    film
+WHERE 
+    special_features = 'Behind the Scenes';
